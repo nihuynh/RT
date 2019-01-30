ba9b4a0e8cfd9d3caf7a144580a848715c4d3ee2
Nicolas Huynh <nico.huynh@gmail.com>
2019-01-30 23:28:21 +0100

    right comand

--- install.sh
+++ install.sh
@@ -6,12 +6,11 @@
 #    By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/01/30 21:38:06 by nihuynh           #+#    #+#              #
-#    Updated: 2019/01/30 22:12:25 by nihuynh          ###   ########.fr        #
+#    Updated: 2019/01/30 22:15:39 by nihuynh          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/nihuynh/RT/master/install.sh"
-# wget -O - https://raw.githubusercontent.com/nihuynh/RT/master/install.sh | sudo bash
+# curl -fsSL https://raw.githubusercontent.com/nihuynh/RT/Nico/install.sh | sh
 
 set -e
 printf "Yo\n"
