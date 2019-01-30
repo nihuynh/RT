9d0f5aa20e6713c8fdae6639088aa74da2f55f19
Nicolas Huynh <nico.huynh@gmail.com>
2019-01-30 22:12:43 +0100

    hum

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@
 #    By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/01/30 21:38:06 by nihuynh           #+#    #+#              #
-#    Updated: 2019/01/30 22:06:37 by nihuynh          ###   ########.fr        #
+#    Updated: 2019/01/30 22:12:25 by nihuynh          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/nihuynh/RT/master/install.sh"
-main()
-{
-	set -e
-	printf "Yo\n"
-}
+# wget -O - https://raw.githubusercontent.com/nihuynh/RT/master/install.sh | sudo bash
+
+set -e
+printf "Yo\n"
