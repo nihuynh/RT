3d37ebd7186c3ce7eb1a7b9172a6809388a2ecdc
nihuynh <nihuynh@student.42.fr>
2019-04-13 01:03:58 +0200

    fixing stuff

--- lib/libui/srcs/itocolor.c
+++ lib/libui/srcs/itocolor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   itocolor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sklepper <sklepper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 20:28:46 by sklepper          #+#    #+#             */
-/*   Updated: 2019/04/12 16:54:22 by sklepper         ###   ########.fr       */
+/*   Updated: 2019/04/13 00:32:58 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	itocolor(t_color *res, int color)
 {
-	res->r = (color & MASK_RED);
+	res->r = ((color & MASK_RED) >> 16);
 	res->g = ((color & MASK_GREEN) >> 8);
-	res->b = ((color & MASK_BLUE) >> 16);
+	res->b = (color & MASK_BLUE);
 }
