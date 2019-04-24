11ebdf0a6b11eac24fec154b216a375cd17d0b43
Nicolas Huynh <nico.huynh@gmail.com>
2019-04-24 13:56:56 +0200

    copy the sources and compile the sdl on travis

--- install_sdl.sh
+++ install_sdl.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 curl -L https://www.libsdl.org/release/SDL2-2.0.9.tar.gz | tar xz
-cd SDL-2.0.9
+cd SDL2-2.0.9
 ./configure
 make
 make install
 cd ..
-rm -rf SDL-2.0.9
+rm -rf SDL2-2.0.9
 echo "SDL2 install successful"
 exit 0
 
