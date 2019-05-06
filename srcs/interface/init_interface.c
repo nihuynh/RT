4dda6177a49729cf304939c70e8fffe6cc910e54
Sansait <sklepper@e3r11p10.42.fr>
2019-05-06 16:09:10 +0200

    Added scene to window title

--- srcs/interface/init_interface.c
+++ srcs/interface/init_interface.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   init_interface.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sklepper <sklepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/12 20:08:33 by sklepper          #+#    #+#             */
-/*   Updated: 2019/04/30 18:09:17 by nihuynh          ###   ########.fr       */
+/*   Updated: 2019/05/06 16:08:14 by sklepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "interface.h"
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl2.h"
+#include "libft.h"
 #if defined(__APPLE__)
 # define GL_SILENCE_DEPRECATION
 # include <OpenGL/gl.h>
@@ -23,6 +24,7 @@
 void	init_interface(t_gui *gui, SDL_Window *window, t_data *app)
 {
 	SDL_GLContext gl_context;
+	char *scene_name;
 
 	gui->log_open = false;
 	gl_context = SDL_GL_CreateContext(window);
@@ -36,4 +38,11 @@ void	init_interface(t_gui *gui, SDL_Window *window, t_data *app)
 	glBindTexture(GL_TEXTURE_2D, 0);
 	gui->light_set = app->scene.lst_light;
 	gui->obj_set = NULL;
+	if (!(scene_name = ft_strrchr(app->arg, '/')))
+		scene_name = app->arg;
+	else
+		scene_name++;
+	scene_name = ft_strjoin("RT - " , scene_name);
+	SDL_SetWindowTitle(window, scene_name);
+	free(scene_name);
 }
