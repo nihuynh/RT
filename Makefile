63e088df2f58a90b054aa5397c4cb66402f1cdd7
nihuynh <nihuynh@student.42.fr>
2019-05-06 18:40:37 +0200

    finish the export

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/09/27 19:33:22 by nihuynh           #+#    #+#              #
-#    Updated: 2019/05/06 18:20:11 by nihuynh          ###   ########.fr        #
+#    Updated: 2019/05/06 18:29:02 by nihuynh          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,7 +21,7 @@ SRC			:=	error.c main.c parser.c read.c render.c parse_toolbox.c	\
 				init_interface.c interface_objects.c interface_tools.c	\
 				object_tools.c render_fullscreen.c texture_toolbox.c \
 				interface_lists.c filters.c export.c export_utils.c \
-				fullscreen.c
+				export_toolbox.c fullscreen.c
 SRC			+=	init_sdl.c error_sdl.c exit_sdl.c\
 				render_sdl.c loop_sdl.c init_mthr_sdl.c render_mthr_sdl.c \
 				save_screenshot.c render_pool.c post_process.c
--- srcs/parser/export_toolbox.c
+++ srcs/parser/export_toolbox.c
@@ -6,16 +6,29 @@
 /*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 17:57:38 by nihuynh           #+#    #+#             */
-/*   Updated: 2019/05/06 17:58:54 by nihuynh          ###   ########.fr       */
+/*   Updated: 2019/05/06 18:29:17 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtstruct.h"
 #include <stdio.h>
 
+static inline void
+	export_color(int fd, char *name, t_color color)
+{
+	dprintf(fd, "\t\t%s(%f %f %f)\n", name, color.r, color.g, color.b);
+}
+
 void
 	export_material(int fd, t_material *mat)
 {
-	dprintf(fd, "\t\ttexture(none)");
-	dprintf(fd, "\t\tcolor(%f %f %f)\n", mat->color.r, mat->color.g, mat->color.b);
+	dprintf(fd, "\t\ttexture(%s)\n", mat->name);
+	export_color(fd, "color_diffuse", mat->color_diffuse);
+	export_color(fd, "color_specular", mat->color_specular);
+	export_color(fd, "self_light", mat->self_light);
+	dprintf(fd, "\t\tspec_idx(%f)\n", mat->spec_idx);
+	dprintf(fd, "\t\tspec_power(%f)\n", mat->spec_power);
+	export_color(fd, "refraction_color", mat->refraction_color);
+	export_color(fd, "reflection_color", mat->reflection_color);
+	dprintf(fd, "\t\trefraction_idx(%f)\n", mat->refraction_idx);
 }
