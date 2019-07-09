661ff3e473a5fa0865e928009b41c579d4aa869f
nihuynh <nihuynh@student.42.fr>
2019-07-09 13:23:51 +0200

    norme

--- lib/librt/includes/csg.h
+++ lib/librt/includes/csg.h
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   csg.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sklepper <sklepper@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nihuynh <nihuynh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 19:02:52 by sklepper          #+#    #+#             */
-/*   Updated: 2019/07/05 19:08:31 by sklepper         ###   ########.fr       */
+/*   Updated: 2019/07/09 13:23:35 by nihuynh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CSG_H
 # define CSG_H
 
-#include "rtstruct.h"
+# include "rtstruct.h"
 
 typedef struct	s_csg_op
 {
 	int			type;
 }				t_csg_op;
 
-typedef struct s_inter_csg t_inter_csg;
+typedef struct s_inter_csg	t_inter_csg;
 
-struct	s_inter_csg
+struct			s_inter_csg
 {
-	t_inter inter;
-	t_inter_csg *next;
+	t_inter		inter;
+	t_inter_csg	*next;
 };
 
 #endif
