dc8ef3dbd9baccdeed42ce06d4e3147f2e0c0c97
Sklepper <sklepper@student.42.fr>
2019-07-13 23:03:35 +0200

    Fix bzero usage

--- srcs/render/light.c
+++ srcs/render/light.c
@@ -6,7 +6,7 @@
 /*   By: sklepper <sklepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 14:41:41 by sklepper          #+#    #+#             */
-/*   Updated: 2019/07/11 12:46:44 by sklepper         ###   ########.fr       */
+/*   Updated: 2019/07/13 23:02:52 by sklepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,8 @@ t_color
 	t_color		diffuse_color;
 	t_color		final_color;
 
-	ft_bzero(&accum_light, sizeof(accum_light));
+	ft_bzero(&accum_light[0], sizeof(accum_light[0]));
+	ft_bzero(&accum_light[1], sizeof(accum_light[1]));
 	current_light = scene.lst_light;
 	while (current_light != NULL)
 	{
