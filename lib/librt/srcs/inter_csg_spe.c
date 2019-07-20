c5c970c4059f447f9e6fc8e85ea8aec918caabc2
Sansait <sklepper@student.42.fr>
2019-07-20 19:29:47 +0200

    Fixed neg plane

--- lib/librt/srcs/inter_csg_spe.c
+++ lib/librt/srcs/inter_csg_spe.c
@@ -6,7 +6,7 @@
 /*   By: sklepper <sklepper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 19:52:48 by sklepper          #+#    #+#             */
-/*   Updated: 2019/07/18 18:38:22 by sklepper         ###   ########.fr       */
+/*   Updated: 2019/07/20 18:48:50 by sklepper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,35 @@ static inline float
 	return (vec3_dot(&postoplane, &plane->n));
 }
 
+static inline t_inter
+	need_more_lines_for_norm(t_inter core, t_inter sub)
+{
+	t_inter	no_inter;
+
+	inter_set(&no_inter, sub.ray);
+	if (core.obj && core.obj->type == 2 && is_inside_cone(&core))
+	{
+		if (sub.dist < core.dist ||
+			(sub.dist > core.dist && sub.dist > core.hit_pts.y))
+			return (sub);
+		if (core.hit_pts.y > 0 && core.hit_pts.y != INFINITY)
+		{
+			core.dist = core.hit_pts.y;
+			core.hit_pts.x = core.hit_pts.y;
+			core.hit_pts.y = INFINITY;
+			return (core);
+		}
+		else
+			return (no_inter);
+	}
+	if (ft_btwf(sub.dist, core.hit_pts.x, core.hit_pts.y))
+		return (sub);
+	if (core.dist < sub.dist)
+		return (no_inter);
+	else
+		return (core);
+}
+
 t_inter
 	not_plane(t_inter core, t_inter sub)
 {
@@ -72,20 +101,18 @@ t_inter
 	var = need_lines_for_norm(plane, sub);
 	if (vec3_dot(&plane->n, &sub.ray.dir) > 0)
 	{
+		if (core.obj && core.obj->type == 2 && is_inside_cone(&core)
+			&& sub.dist < core.dist)
+			return (sub);
 		if (var > 0)
 			return (no_inter);
-		else if (core.dist < sub.dist || sub.dist < 0)
+		if (core.dist < sub.dist || sub.dist < 0)
 			return (core);
-	}
-	if (var > 0)
-	{
 		if (ft_btwf(sub.dist, core.hit_pts.x, core.hit_pts.y))
 			return (sub);
-		if (core.dist < sub.dist)
-			return (no_inter);
-		else
-			return (core);
 	}
+	else if (var > 0)
+		return (need_more_lines_for_norm(core, sub));
 	if (sub.dist == INFINITY && var < 0)
 		return (core);
 	return (no_inter);
